cac66735c57ef055f1680750a05ea898ea6c9023
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-29 15:59:34 +0300

    Add kitchen-process model

--- kitchen/models.py
+++ kitchen/models.py
@@ -16,3 +16,11 @@ class Cook(AbstractUser):
                 f"Name: {self.first_name} "
                 f"Surname: {self.last_name})"
                 )
+
+
+class KitchenProcess(models.Model):
+    process = models.CharField(max_length=255, unique=True, blank=False)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Process: {self.process}"
