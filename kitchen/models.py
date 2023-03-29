a8c86e716d81cbf6cd03433cd8d5a9aa5a8efa87
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-29 15:45:53 +0300

    Add cook model that inherits from AbstractUser

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+    kitchen_process = models.ManyToManyField(KitchenProcess, related_name="cooks")
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self) -> str:
+        return (f"Username: {self.username} "
+                f"Name: {self.first_name} "
+                f"Surname: {self.last_name})"
+                )
