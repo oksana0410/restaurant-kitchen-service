c70a73b8ec1b20cb73ef3c2df45f16aa3a8b78ac
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-29 20:17:16 +0300

    Add visits counter, add empty index.html

--- kitchen/views.py
+++ kitchen/views.py
@@ -9,10 +9,14 @@ def index(request):
     num_dishes = Dish.objects.count()
     num_dish_types = DishType.objects.count()
 
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_cooks": num_cooks,
         "num_dishes": num_dishes,
-        "num_dish_types": num_dish_types
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
     }
 
     return render(request, "kitchen/index.html", context=context)
