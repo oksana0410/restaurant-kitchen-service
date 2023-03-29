cbdd90c75d27fbde71cda13f0085751c9cd8c1e9
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-29 16:16:46 +0300

    Modify dish-type model

--- kitchen/models.py
+++ kitchen/models.py
@@ -2,9 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class KitchenProcess(models.Model):
+    process = models.CharField(max_length=255, unique=True, blank=False)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Process: {self.process}"
+
+
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(null=True)
-    kitchen_process = models.ManyToManyField(KitchenProcess, related_name="cooks")
+    kitchen_process = models.ManyToManyField(KitchenProcess, prefetch_related="cooks")
 
     class Meta:
         ordering = ["username"]
@@ -18,16 +26,9 @@ class Cook(AbstractUser):
                 )
 
 
-class KitchenProcess(models.Model):
-    process = models.CharField(max_length=255, unique=True, blank=False)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return f"Process: {self.process}"
-
-
 class DishType(models.Model):
     name = models.CharField(max_length=255)
+    kitchen_process = models.ForeignKey(KitchenProcess, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Dish type: {self.name}"
