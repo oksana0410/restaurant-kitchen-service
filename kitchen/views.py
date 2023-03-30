7ccf3d641d271268a98325aa210bb10cbf4c4c83
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-30 15:15:32 +0300

    Add dish detail view, add search_form to context for dish

--- kitchen/views.py
+++ kitchen/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from kitchen.forms import CookForm, DishForm, DishTypeForm, DishTypeSearchForm
+from kitchen.forms import CookForm, DishForm, DishTypeForm, DishTypeSearchForm, DishSearchForm
 from kitchen.models import Cook, Dish, DishType
 
 
@@ -101,6 +101,30 @@ class CookDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = DishSearchForm(
+            initial={"name": name}
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = Dish.objects.all()
+        form = DishSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
+        return queryset
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
 
 
 class DishCreateView(LoginRequiredMixin, generic.CreateView):
