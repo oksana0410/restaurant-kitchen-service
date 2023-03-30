b41c718b7f100a0e8df3a6381413c2230bd0b5f9
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-30 14:39:56 +0300

    Add search forms for models

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -26,3 +26,30 @@ class DishTypeForm(forms.ModelForm):
     class Meta:
         model = DishType
         fields = ["name"]
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class DishSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
