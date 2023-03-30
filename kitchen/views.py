fc7b51488247133c994bea20d67cb5a42f671b68
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-30 15:24:41 +0300

    Add cook detail view, add search_form to context for cook

--- kitchen/views.py
+++ kitchen/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from kitchen.forms import CookForm, DishForm, DishTypeForm, DishTypeSearchForm, DishSearchForm
+from kitchen.forms import CookForm, DishForm, DishTypeForm, DishTypeSearchForm, DishSearchForm, CookSearchForm
 from kitchen.models import Cook, Dish, DishType
 
 
@@ -79,6 +79,32 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all()
+        form = CookSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+
+        return queryset
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.prefetch_related("dishes__dish_type")
 
 
 class CookCreateView(LoginRequiredMixin, generic.CreateView):
