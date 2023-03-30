907a96fd4f619b4ecd8d01732ecabb4768f594ce
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-03-30 15:00:20 +0300

    Add dish type detail view and template

--- kitchen/views.py
+++ kitchen/views.py
@@ -28,6 +28,7 @@ def index(request):
 class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dish_type_list"
+    template_name = "kitchen/dish_type_list.html"
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -49,6 +50,11 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
             )
 
         return queryset
+
+
+class DishTypeDetailView(LoginRequiredMixin, generic.DetailView):
+    model = DishType
+    template_name = "kitchen/dish_type_detail.html"
 
 
 class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
